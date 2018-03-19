70f19b5bffd22ef2a78ec02b044fe98f65417e08
Alapan Kar <alapankar27@gmail.com>
2018-03-19 10:12:48 +0530

    Update background.js

--- toPDF/background.js
+++ toPDF/background.js
@@ -4,22 +4,18 @@ browser.tabs.onActivated.addListener(listenWhenTabChange);
 
 browser.tabs.onCreated.addListener(listenWhenTabCreated);
 
-// Come to this method when the tab url gets changed
 function listenForTabChange(tabId, changeInfo, tab) {
-// Get the current Tab details.
+
 		 console.log(' tab url is changed');
 	getCurrentTabDetails(); 
 }
 
-// Come to this method when the tab gets changed.
 function listenWhenTabChange (activeInfo) {
-// Get the current Tab details.
 	 console.log(' tab is changed');
 	
 getCurrentTabDetails();
 }
 
-// When the tab is created we will get tab object, from tab object itself we can get the tab id and show directly
 function listenWhenTabCreated(tab){
 	console.log('tab is created');
 	browser.pageAction.show(tab.id)
@@ -31,12 +27,6 @@ function getCurrentTabDetails(){
 	querying.then(getInfoForTab, onError);
 }
 
-/* 
-Once we get the tab Information of current tab, come here.
-get the current tab object.
-check current tab is Article type.
-	if Article type then show the pageAction icon.
-*/
 function getInfoForTab(tabs) {
 
   if (tabs.length > 0) {
@@ -52,16 +42,8 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-
-
-/*
-	After the user clicked the small page action icon, we will be listening at doPageActClickProc
-*/
 browser.pageAction.onClicked.addListener(doPageActClickProc);
 
-
-
-// Bring in the save as PDF pop-up
 function doPageActClickProc(){
 	 //browser.tabs.toggleReaderMode();	
 	 browser.tabs.saveAsPDF({});
